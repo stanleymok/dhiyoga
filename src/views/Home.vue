3b184db9ab1c6dac1892de53bc133cb619ab5d0e
stanleymok <stanleymwork@gmail.com>
2021-04-22 02:27:54 +0800

    feat: update logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/dhiyoga_logo.png">
     <HelloWorld msg="DHIYOGA"/>
   </div>
 </template>
