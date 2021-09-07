23248b6b7cdbb00e36a0da7576ee48f0132ed654
Stanley Joseph Mok <e0191576@u.nus.edu>
2021-09-08 01:02:39 +0800

    Feat stylise search bar (#2)
    * feat: add contact us page
    
    * feat: clean front page! add contact us page
    
    * feat: update logo
    
    * refactor: remove redundant code
    
    * feat: add dynamic pagesgit diff --staged --name-only! (:
    
    * feat: add backendgit diff --staged --name-only semi working
    
    * feat: prettify pages
    
    * feat: fix app.py, pipfile, pipfile.lock
    
    * chore: update readme
    
    * feat: set backend app to port 8081
    
    * bug: fix backend error!
    
    * bug: fix backend error!

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is a page on the developers; how to contact us</h1>
+    <h4>Stanley</h4>
+    <h5>Team Leader; Full Stacker; Repository Starter</h5>
+    <h5>HP: 96651408, MAIL: stanleymwork@gmail.com</h5>
+    <h4>Sophia</h4>
+    <h5>Straggler; Front Ender; Beautifier</h5>
+    <h5>HP: 85187744, MAIL: sophiaMsimangan@gmail.com</h5>
   </div>
 </template>
